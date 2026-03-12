8e23e10e73f43d8a407cbb465d5673db842a5918
Naflan-MNM <naflanm084@gmail.com>
2026-03-12 15:45:53 +0530

    update json middleware and db connection logic

--- server.js
+++ server.js
@@ -8,13 +8,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use(express.json());
 app.use("/auth/api", authroutes);
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`server running on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("DB connected fail", error);
+  }
+};
+
+startServer();
