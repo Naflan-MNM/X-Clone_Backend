40faac6402f014ad5fd173c15b505ad06129c5dc
Naflan-MNM <naflanm084@gmail.com>
2026-03-12 15:46:13 +0530

    update routes

--- routes/Routes.js
+++ routes/Routes.js
@@ -3,7 +3,7 @@ import { signup, login, logout } from "../controller/auth.controler.js";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.get("/login", login);
 router.post("/signup", signup);
 router.post("/logout", logout);
 
