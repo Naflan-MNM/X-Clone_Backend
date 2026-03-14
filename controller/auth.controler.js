e9144b10d25480ced038b76b4396725ce028439e
Naflan-MNM <naflanm084@gmail.com>
2026-03-14 23:32:18 +0530

    update signup controller and  implement logout, getme controller

--- controller/auth.controler.js
+++ controller/auth.controler.js
@@ -2,50 +2,43 @@ import User from "../model/schema/UserSchema.js";
 import bcrypt from "bcryptjs";
 import validator from "validator";
 import generateToken from "../utils/generateToken.js";
-import { mongo } from "mongoose";
+import protectRoute from "../middleware/protectRoute.js";
 
+/* signup controller */
 const signup = async (req, res) => {
   try {
     const { username, fullname, password, email } = req.body;
-
     if (!username || !fullname || !email || !password) {
       return res.status(400).json({
         error: "All fields are required",
       });
     }
-
     if (!validator.isEmail(email)) {
       return res.status(400).json({
         error: "Invalid email format",
       });
     }
     //still your email validator not working properly as you expect
-
     if (password.length < 6) {
       return res.status(400).json({
         error: "Password must be at least 6 characters",
       });
     }
-
     const existingUser = await User.findOne({
       $or: [{ email }, { username }],
     });
-
     if (existingUser) {
       return res.status(400).json({
         error: "Username or email already exists",
       });
     }
-
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUser = new User({
       username,
       fullname,
       email,
       password: hashedPassword,
     });
-
     if (newUser) {
       generateToken(newUser._id, res);
       await newUser.save();
@@ -60,17 +53,55 @@ const signup = async (req, res) => {
     });
   }
 };
+
+/* login controller */
 const login = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const findUsername = await User.find(username);
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({
+        error: "Invalid username",
+      });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        error: "Invalid password",
+      });
+    }
+    generateToken(user._id, res);
+    return res.status(200).json({
+      message: "Login successful",
+    });
   } catch (error) {
-    console.error(`Error in login controller:${error}`);
-    res.status(500).json({ error: "Internal server errorr" });
+    console.error(`Error in login controller: ${error}`);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
-const logout = (req, res) => {
-  res.send("this  is logout");
+
+/* logout controller */
+const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", {
+      maxAge: 0,
+    });
+    return res.status(200).json({ message: "logout successfully" });
+  } catch (error) {
+    console.error(`Error in logout controller: ${error}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-export { signup, login, logout };
+/* getMe controller */
+const getMe = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.user._id }).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(`Error in getMe controller: ${error}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export { signup, login, logout, getMe };
