370198077bc2fdea2c09a56105130afd5167b780
Naflan-MNM <naflanm084@gmail.com>
2026-03-14 23:34:54 +0530

    updates the generatetoken function

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,10 +7,9 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 1000,
     httpOnly: true, //it will prevent the attack of xss
-    smaeSite: "strict", // this is also prevent the attack of CSRF
+    sameSite: "strict", // this is also prevent the attack of CSRF
     secure: process.env.NODE_ENV !== "development",
   });
-  //console.log(process.env.JWT_SECRET);
 };
 
 export default generateToken;
