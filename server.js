7ca91d274cc0dd18fb9fc7ee11fe06003dbac8f7
Naflan-MNM <naflanm084@gmail.com>
2026-03-16 06:16:25 +0530

    import user routes and update the path of auth routes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import authroutes from "./routes/Routes.js";
+import authroutes from "./routes/auth.route.js";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import userroutes from "./routes/user.routes.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3500;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/auth/api", authroutes);
+app.use("/auth/users", userroutes);
 
 app.get("/", (req, res) => {
   res.send("hello world!");
