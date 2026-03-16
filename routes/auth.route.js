4f665c77a7ec9d12f353ff1d947a538f7167b541
Naflan-MNM <naflanm084@gmail.com>
2026-03-16 06:19:35 +0530

    renamed previous name routes into auth.routes

---
+++ routes/auth.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { signup, login, logout, getMe } from "../controller/auth.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
+
+const router = express.Router();
+
+router.post("/login", login);
+router.post("/signup", signup);
+router.post("/logout", logout);
+router.get("/me", protectRoute, getMe);
+
+export default router;
