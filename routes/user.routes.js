20138bd80c523f6acfba31628e615f6e7cee4177
Naflan-MNM <naflanm084@gmail.com>
2026-06-10 10:15:22 +0530

    import followunfollowuser and getsuggesteduser functions and added two  userroutes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getProfile } from "../controller/user.controller.js";
+import {
+  getProfile,
+  followUnFollowUsers,
+  getSuggestedUser,
+} from "../controller/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getProfile);
+router.post("/follow/:id", protectRoute, followUnFollowUsers);
+router.get("/suggested", protectRoute, getSuggestedUser);
 
 export default router;
