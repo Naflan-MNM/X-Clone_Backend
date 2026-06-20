24af63b64491d8c4cd071dd632e0d5957d48f050
Naflan-MNM <naflanm084@gmail.com>
2026-06-20 23:09:37 +0530

    update the error messages

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -60,7 +60,7 @@ const login = async (req, res) => {
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(400).json({
-        error: "Invalid username",
+        error: "Enter the user name",
       });
     }
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
@@ -73,6 +73,7 @@ const login = async (req, res) => {
     return res.status(200).json({
       message: "Login successful",
     });
+    //return res.status(200).json(user);
   } catch (error) {
     console.error(`Error in login controller: ${error}`);
     res.status(500).json({ error: "Internal server error" });
