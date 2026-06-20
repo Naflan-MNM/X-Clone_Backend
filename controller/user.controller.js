e687235dd2e8c6c9dd00e06ad4224f0ec6c0cc93
Naflan-MNM <naflanm084@gmail.com>
2026-06-20 23:10:45 +0530

    import and devloped the userupdate fucntiona and libraires

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,9 @@
 import User from "../model/schema/UserSchema.js";
 import Notification from "../model/notification.model.js";
+import bcrypt from "bcryptjs";
+import cloudinary from "cloudinary";
+import validator from "validator";
+
 const getProfile = async (req, res) => {
   try {
     console.log("USERNAME PARAM:", req.params.username);
@@ -78,12 +82,102 @@ const getSuggestedUser = async (req, res) => {
         $nin: currentUser.following, // exclude already followed
       },
     }).limit(10);
+    suggestedUsers.forEach((user) => {
+      user.password = null;
+    });
 
     res.status(200).json(suggestedUsers);
   } catch (error) {
-    console.error(`error: ${error}`);
+    console.error(`error from suggestedUser controller: ${error}`);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-export { getProfile, followUnFollowUsers, getSuggestedUser };
+const updateUser = async (req, res) => {
+  try {
+    const userid = req.user._id;
+    if (!userid) {
+      return res.status(400).json({ erros: "user not found" });
+    }
+    let currentUser = await User.findById(userid);
+    let {
+      fullname,
+      email,
+      currentPassword,
+      newPassword,
+      bio,
+      link,
+      profileImage,
+      coverImage,
+    } = req.body;
+    if (newPassword || currentPassword) {
+      if (!currentPassword && newPassword) {
+        return res
+          .status(400)
+          .json({ error: "pleace provide Current Password" });
+      }
+      if (!newPassword && currentPassword) {
+        return res.status(400).json({ error: "pleace provide New Password" });
+      }
+      const isMatchPassword = await bcrypt.compare(
+        currentPassword,
+        currentUser.password,
+      );
+      if (!isMatchPassword) {
+        return res.status(400).json({ error: "Invalid Password" });
+      }
+      if (newPassword.length < 6) {
+        return res.status(400).json({
+          error: "Password must be at least 6 characters",
+        });
+      }
+    }
+    const updatePassword = async (req, res) => {
+      const newhashPassword = await bcrypt.hash(req.password, 10);
+      currentUser.password = newhashPassword || currentUser.password;
+    };
+    if (email && !validator.isEmail(email)) {
+      return res.status(400).json({
+        error: "Please enter a valid email address",
+      });
+    }
+
+    if (profileImage) {
+      if (currentUser.profileimage) {
+        //the cloudinary secure_url will be look like https://res.cloudinary.com/jhgfuo5j/image/upload/v1254879995/cld-sample-5.jpg
+        /*from here we need that path name which is 'cld-sample-5' to get that you used the below code snipet*/
+        await cloudinary.uploader.destroy(
+          currentUser.profileimage.split("/").pop().split(".")[0],
+        );
+      }
+      const uploadedResponse = await cloudinary.uploader.upload(profileImage);
+      const newProfileImgUrl = uploadedResponse.secure_url;
+      currentUser.profileImage = newProfileImgUrl || currentUser.profileImage;
+    }
+
+    if (coverImage) {
+      if (currentUser.coverImage) {
+        await cloudinary.uploader.destroy(
+          currentUser.coverImage.split("/").pop().split(".")[0],
+        );
+      }
+      const uploadedResponse = await cloudinary.uploader.upload(coverImage);
+      const newCoverImageUrl = uploadedResponse.secure_url;
+      currentUser.coverImage = newCoverImageUrl || currentUser.coverImage;
+    }
+
+    currentUser.fullname = fullname || currentUser.fullname;
+    currentUser.email = email || currentUser.email;
+    currentUser.bio = bio || currentUser.bio;
+    currentUser.link = link || currentUser.link;
+
+    currentUser = await currentUser.save();
+    currentUser.password = null;
+    return res.status(200).json(currentUser);
+  } catch (error) {
+    console.error(`error from updateUser controller: ${error}`);
+    res.status(500).json({ error: "iternal server error" });
+  }
+};
+
+export { getProfile, followUnFollowUsers, getSuggestedUser, updateUser };
