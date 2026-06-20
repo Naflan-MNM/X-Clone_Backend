4c6f9b24f3cdd4fb3defcdc656d016e6fde39f77
Naflan-MNM <naflanm084@gmail.com>
2026-06-20 23:08:51 +0530

    import cloudinary library

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ import authroutes from "./routes/auth.route.js";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userroutes from "./routes/user.routes.js";
+import cloudinary from "cloudinary";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3500;
+cloudinary.config({
+  cloude_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+});
 
 app.use(express.json());
 app.use(cookieParser());
